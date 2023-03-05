5b393d5f81700a95286c287d56cf957e850624ca
Nikolay Povstyanko <povstyankonikolai@gmail.com>
2023-03-05 19:30:19 +0200

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,25 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    es6: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    requireConfigFile: false,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/require-default-prop': 'off',
   },
+  ignorePatterns: ['node_modules', 'build', 'dist', 'public'],
 };
